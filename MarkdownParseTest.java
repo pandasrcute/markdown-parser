10dee0c74a08947e59adef01b38dd2209ff748d3
Hyungjun Doh <augustinedoh@gmail.com>
2022-04-24 01:58:33 -0700

    Writing tests with JUnit
    Lab 4 - Part 4 - Writing tests with JUnit

---
+++ MarkdownParseTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1+1);
+    }
+    
+}
