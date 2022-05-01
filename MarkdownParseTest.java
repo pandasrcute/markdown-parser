a3082d6ce9db8eb6e8830293ad58f577e0edfb91
pandasrcute <tzhaobhs@gmail.com>
2022-04-30 17:53:38 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,30 +25,37 @@ public class MarkdownParseTest {
         ArrayList<String> actual = MarkdownParse.getLinks(contents);
         assertEquals(expected,actual);
     }
-    /*
     @Test
     public void testgetLinks2() throws IOException {
         String filename = "test-file1.md";
         Path path2filename = Paths.get(filename);
         String contents = Files.readString(path2filename);
-        ArrayList<String> expected = new ArrayList(List.of("https://something.com", "some-thing.html"));
-        assertEquals(expected,MarkdownParse.getLinks(contents));
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        ArrayList<String> actual = MarkdownParse.getLinks(contents);
+        assertEquals(expected,actual);
     }
     @Test
     public void testgetLinks3() throws IOException {
         String filename = "test-file2.md";
         Path path2filename = Paths.get(filename);
         String contents = Files.readString(path2filename);
-        ArrayList<String> expected = new ArrayList(List.of("some-thing.html"));
-        assertEquals(expected,MarkdownParse.getLinks(contents));
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("some-thing.html");
+        ArrayList<String> actual = MarkdownParse.getLinks(contents);
+        assertEquals(expected,actual);
     }
     @Test
     public void testgetLinks4() throws IOException {
         String filename = "test-file3.md";
         Path path2filename = Paths.get(filename);
         String contents = Files.readString(path2filename);
-        ArrayList<String> expected = new ArrayList(List.of("https://something.com", "some-thing.html"));
-        assertEquals(expected,MarkdownParse.getLinks(contents));
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+        ArrayList<String> actual = MarkdownParse.getLinks(contents);
+        assertEquals(expected,actual);
     }
-    */
+    
 }
